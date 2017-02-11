f83d74de9dc7483d42427a58c8281301bfe9c584
AlexDenisov <1101.debian@gmail.com>
2017-02-11 23:46:39 +0100

    Add default themes

--- main.c
+++ main.c
@@ -49,11 +49,12 @@ void apply_theme(const int color_index, const theme t, FILE *binary) {
 void change_color(color c, FILE *binary) {
   fseek(binary, ColorBase, SEEK_SET);
   fwrite(&c, sizeof(color), 1, binary);
-  perror("fwrite");
   rewind(binary);
 }
 
-theme yellow_theme() {
+/// Default Themes
+
+theme default_yellow_theme() {
   color background_color = { 0.996078, 0.956862, 0.611764 };
   color border_color = { 0.737255, 0.662745, 0.007843 };
   color window_title_color = { 0.996078, 0.917647, 0.239216 };
@@ -61,6 +62,65 @@ theme yellow_theme() {
 
   theme t = { background_color, border_color, window_title_color, icon_color };
   return t;
+}
+
+theme default_blue_theme() {
+  color background_color = { 0.678431, 0.956863, 1 };
+  color border_color = { 0.007843, 0.737255, 0.843137 };
+  color window_title_color = { 0.537255, 0.941176, 1 };
+  color icon_color = { 0.141176, 0.815686, 0.913725 };
+
+  theme t = { background_color, border_color, window_title_color, icon_color };
+  return t;
+}
+
+theme default_green_theme() {
+  color background_color = { 0.698039, 1, 0.631373 };
+  color border_color = { 0.282353, 0.635294, 0.282353 };
+  color window_title_color = { 0.513725, 0.996078, 0.513725 };
+  color icon_color = { 0.317647, 0.733333, 0.317647 };
+
+  theme t = { background_color, border_color, window_title_color, icon_color };
+  return t;
+}
+
+theme default_pink_theme() {
+  color background_color = { 1, 0.780392, 0.780392 };
+  color border_color = { 0.886275, 0.458824, 0.458824 };
+  color window_title_color = { 1, 0.698039, 0.698039 };
+  color icon_color = { 0.972549, 0.498039, 0.498039 };
+
+  theme t = { background_color, border_color, window_title_color, icon_color };
+  return t;
+}
+
+theme default_purple_theme() {
+  color background_color = { 0.713725, 0.792157, 1 };
+  color border_color = { 0.458824, 0.568627, 0.862745 };
+  color window_title_color = { 0.607843, 0.713725, 0.996078 };
+  color icon_color = { 0.490196, 0.607843, 0.921569 };
+
+  theme t = { background_color, border_color, window_title_color, icon_color };
+  return t;
+}
+
+theme default_gray_theme() {
+  color background_color = { 0.933333, 0.933333, 0.933333 };
+  color border_color = { 0.619608, 0.619608, 0.619608 };
+  color window_title_color = { 0.854902, 0.854902, 0.854902 };
+  color icon_color = { 0.658824, 0.658824, 0.658824 };
+
+  theme t = { background_color, border_color, window_title_color, icon_color };
+  return t;
+}
+
+void apply_default_themes(FILE *binary) {
+  apply_theme(0, default_yellow_theme(), binary);
+  apply_theme(1, default_blue_theme(), binary);
+  apply_theme(2, default_green_theme(), binary);
+  apply_theme(3, default_pink_theme(), binary);
+  apply_theme(4, default_purple_theme(), binary);
+  apply_theme(5, default_gray_theme(), binary);
 }
 
 color red_color() {
@@ -76,7 +136,7 @@ int main(int argc, char **argv) {
     return 1;
   }
 
-  apply_theme(0, yellow_theme(), binary);
+  apply_default_themes(binary);
 
   fclose(binary);
 
